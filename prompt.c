89d43437d1941d8f109dce73110af6dcdec38486
MHRasmy <18063@stemegypt.edu.eg>
2023-04-27 14:59:06 -0700

    fixed non-interactive mode

--- prompt.c
+++ prompt.c
@@ -10,14 +10,19 @@ void prompt(char **argv __attribute__((unused)), char **env)
 {
 	char *line = NULL, **args;
 	size_t len = 0;
+	int is_terminal = isatty(STDIN_FILENO);
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO))
+		if (is_terminal)
 			printf("%s", PROMPT);
 
 		if (getline(&line, &len, stdin) == -1)
-			perror("ERROR");
+		{
+			if (is_terminal)
+                                perror("ERROR");
+			break;
+		}
 
 		if (line[strlen(line) - 1] == '\n')
 			line[strlen(line) - 1] = '\0';
@@ -27,13 +32,13 @@ void prompt(char **argv __attribute__((unused)), char **env)
 			exit_shell(args);
 		else if (strcmp(args[0], "env") == 0)
 			env_shell(env);
-		else
-			execute_command(args, env);
+		if (execute_command(args, env))
+			continue;
 
 		free(args);
+		free(line);
+		line = NULL;
 	}
-
-	free(line);
 }
 
 /**
@@ -54,11 +59,11 @@ void exit_shell(char **av)
  */
 void env_shell(char **env)
 {
-    int i = 0;
+	int i = 0;
 
-    while (env[i] != NULL)
-    {
-        printf("%s\n", env[i]);
-        i++;
-    }
+	while (env[i] != NULL)
+	{
+		printf("%s\n", env[i]);
+		i++;
+	}
 }
