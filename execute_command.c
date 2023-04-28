4a4a835138d3cafbda0e48223139871df1764fc9
MHRasmy <18063@stemegypt.edu.eg>
2023-04-28 15:43:15 -0700

    removing getline()

--- execute_command.c
+++ execute_command.c
@@ -6,17 +6,18 @@
  * @env: environment variables
  * Return: 0 on success and 1 on failure
  */
-void execute_command(char **args, char **env __attribute__((unused)), char *arg)
+int execute_command(char **args, char **env __attribute__((unused)), char *arg)
 {
 	int i = 0;
 	pid_t pid;
+	int status;
 
 	for (i = 0; i < builtins_size(); i++)
 	{
 		if (_strcmp(args[0], builtins[i].name) == 0)
 		{
 			builtins[i].func(args);
-			return;
+			return (0);
 		}
 	}
 
@@ -25,15 +26,18 @@ void execute_command(char **args, char **env __attribute__((unused)), char *arg)
 
 	if (pid == 0)
 	{
-		execvp(args[0], args);
-		_perror(arg);
-		_perror(": No such file or directory\n");
+		if (execvp(args[0], args) < 0)
+		{
+			_perror(arg);
+			_perror(": No such file or directory\n");
+		}
 		exit(1);
 	}
 	else if (pid > 0)
 	{
-		wait(NULL);
+		waitpid(pid, & status, WUNTRACED);
 	}
 	else if (pid < 0)
 		_perror("failed to fork\n");
+	return (1);
 }
