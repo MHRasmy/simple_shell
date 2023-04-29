dd7c654f9939ac18b2c523f89894ce9b048ffc1d
MHRasmy <18063@stemegypt.edu.eg>
2023-04-29 14:02:50 -0700

    betty

--- shell.h
+++ shell.h
@@ -39,7 +39,7 @@ void env_shell(void);
 /* executing functions */
 int mul(int num1, int num2);
 void prompt(char **argv, char **env);
-void execute_command(char **args, char **env, char *arg);
+void execute_command(char **args, char *arg);
 
 /* parsing input */
 char **tokenize(char *line);
