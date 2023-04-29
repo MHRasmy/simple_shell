3db064bf635f1fdcd98af8100184994fbc82bbde
MHRasmy <18063@stemegypt.edu.eg>
2023-04-28 19:41:40 -0700

    handled PATH

--- execute_command.c
+++ execute_command.c
@@ -11,6 +11,7 @@ void execute_command(char **args, char **env __attribute__((unused)), char *arg)
 	int i = 0;
 	pid_t pid;
 	int status;
+	char *path;
 
 	for (i = 0; i < builtins_size(); i++)
 	{
@@ -23,20 +24,55 @@ void execute_command(char **args, char **env __attribute__((unused)), char *arg)
 
 	pid = fork();
 
-
-	if (pid == 0)
+	path = search_path(args[0]);
+	if (access(path, X_OK) == 0)
 	{
-		if (execve(args[0], args, env) < 0)
+		if (pid == 0)
 		{
-			_perror(arg);
-			_perror(": No such file or directory\n");
-			exit(1);
+			if (execve(path, args, env) < 0)
+			{
+				_perror(arg);
+				_perror(": No such file or directory\n");
+				exit(1);
+			}
 		}
+		else if (pid > 0)
+		{
+			waitpid(pid, &status, WUNTRACED);
+		}
+		else if (pid < 0)
+			_perror("failed to fork\n");
 	}
-	else if (pid > 0)
+	free(path);
+}
+
+char* search_path(char *filename) {
+	char *path, *path_env, *full_path;
+	if (filename[0] == '/')
 	{
-		waitpid(pid, &status, WUNTRACED);
+		if (access(filename, F_OK) == 0)
+			return realpath(filename, NULL);
+		else
+			return NULL;
 	}
-	else if (pid < 0)
-		_perror("failed to fork\n");
+
+	path_env = malloc(MAX_PATH_LEN * sizeof(char));
+	snprintf(path_env, MAX_PATH_LEN, "/bin:%s", getenv("PATH"));
+	path = strtok(path_env, ":");
+	full_path = malloc(MAX_PATH_LEN * sizeof(char));
+
+	while (path != NULL)
+	{
+		snprintf(full_path, MAX_PATH_LEN, "%s/%s", path, filename);
+		if (access(full_path, F_OK) == 0)
+		{
+			free(path_env);
+			return realpath(full_path, NULL);
+		}
+		path = strtok(NULL, ":");
+	}
+
+	free(path_env);
+	free(full_path);
+	return NULL;
 }
